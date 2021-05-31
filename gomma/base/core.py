538812ef655ebbfbb0bd4d8f956cdfd55f5a400b
b3b8 <d.pellegrino@hotmail.it>
2021-05-31 15:30:20 +0200

    deprecated eb2,upg base

--- gomma/base/core.py
+++ gomma/base/core.py
@@ -111,3 +111,79 @@ class Base(object):
         agent = self.s.getAgent()
         r = agent.get(rq, params=params)
         return self.s.response(r)
+
+
+    # company
+    def getCompany(self, company_id: int, params={}):
+        """
+        Legge un company dal suo id.
+        """
+        logging.info(f'Get company {company_id}')
+        rq = f'{self.host}/company/{company_id}'
+        try:
+            agent = self.s.getAgent()
+            r = agent.get(rq, params=params)
+        except Exception:
+            logging.error(f'Failed request {rq}')
+            return False
+        return self.s.response(r)
+
+    def getCompanies(self, params={}):
+        """
+        Prende tutti gli companies.
+        """
+        logging.info('Getting all the companies')
+        rq = f'{self.host}/company'
+        try:
+            agent = self.s.getAgent()
+            r = agent.get(rq, params=params)
+        except Exception:
+            logging.error(f'Failed request {rq}')
+            return False
+        return self.s.response(r)
+
+    def createCompany(self, payload):
+        """
+        Create new company.
+        """
+        logging.info(f'Creating company {payload}')
+        rq = f'{self.host}/company'
+        try:
+            agent = self.s.getAgent()
+            r = agent.post(rq, json=payload)
+        except Exception:
+            logging.error(f'Failed request {rq}')
+            return False
+        return self.s.response(r)
+
+    def getCompanyFromCode(self, code: str, params={}):
+        """
+        Get company from code.
+        """
+        logging.info(f'Search company code {code}.')
+        query = {
+            'code': code
+        }
+        payload = {**params, **query}
+        rq = f'{self.host}/company/findByCode'
+        try:
+            agent = self.s.getAgent()
+            r = agent.get(rq, params=payload)
+        except Exception:
+            logging.error(f'Failed request {rq}')
+            return False
+        return self.s.response(r)
+
+    def updateCompany(self, company_id: int, payload):
+        """
+        Update company.
+        """
+        logging.info(f'Updating company {company_id} with {payload}')
+        rq = f'{self.host}/company/{company_id}'
+        try:
+            agent = self.s.getAgent()
+            r = agent.post(rq, json=payload)
+        except Exception:
+            logging.error(f'Failed request {rq}')
+            return False
+        return self.s.response(r)
